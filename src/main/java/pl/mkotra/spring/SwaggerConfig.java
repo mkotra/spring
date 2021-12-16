824e379393774ce7d8e2af040fbee5fc209f35a1
mkotra <kotra.michal@gmail.com>
2021-12-16 23:52:48 +0100

    bump dependencies

--- src/main/java/pl/mkotra/spring/SwaggerConfig.java
+++ src/main/java/pl/mkotra/spring/SwaggerConfig.java
@@ -1,40 +1,17 @@
 package pl.mkotra.spring;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.net.URI;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Profile("!tests")
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
-    public RouterFunction<ServerResponse> redirect() {
-        return route(
-                GET("/"),
-                req -> ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build()
-        );
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("pl.mkotra.spring"))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info().title("Spring"));
     }
 }
