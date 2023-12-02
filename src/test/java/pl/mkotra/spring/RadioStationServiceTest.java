c7661b2f829334502f91fdcc789d80783a4d3a02
mkotra <kotra.michal@gmail.com>
2023-12-02 23:12:04 +0100

    introduce jacoco code coverage report

--- src/test/java/pl/mkotra/spring/RadioStationServiceTest.java
+++ src/test/java/pl/mkotra/spring/RadioStationServiceTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class ItemServiceTest {
+public class RadioStationServiceTest {
 
     RadioStationRepository radioStationRepository = mock(RadioStationRepository.class);
     RadioBrowserAdapter radioBrowserAdapter = mock(RadioBrowserAdapter.class);
