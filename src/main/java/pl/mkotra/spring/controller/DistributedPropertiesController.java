f4c551e6f15b7147868a21e5590bc57dd39253d1
mkotra <kotra.michal@gmail.com>
2024-06-27 13:22:55 +0200

    dependency update

--- src/main/java/pl/mkotra/spring/controller/DistributedPropertiesController.java
+++ src/main/java/pl/mkotra/spring/controller/DistributedPropertiesController.java
@@ -2,14 +2,16 @@ package pl.mkotra.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.mkotra.spring.DistributedProperties;
 
 @RestController
+@RefreshScope
 public class DistributedPropertiesController {
 
-    @Value("${distributed.property:none}")
+    @Value("${distributed.property}")
     String value;
 
     @Autowired
